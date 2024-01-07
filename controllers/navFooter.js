079dbdea03070c031ebf973db4854cd82824ede4
Tushar Mishra <tusharmishra9695@gmail.com>
2024-01-08 00:44:14 +0530

    koi update hua hai

--- controllers/navFooter.js
+++ controllers/navFooter.js
@@ -3,7 +3,7 @@ const NavFooter = require("../schemas/NavFooterSchema");
 async function handleGetNavFooter(req, res) {
   try {
     let navFooterData = await NavFooter.find();
-    res.send({ navFooterData, message: "hello here is nav footer data" });
+    res.send(navFooterData);
   } catch (error) {
     console.log(error, "error");
   }
